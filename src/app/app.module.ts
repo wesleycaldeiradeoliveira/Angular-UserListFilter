2124fadb94119e18a540375ec8bba384a31215dc
wesleycaldeiradeoliveira <wesleycaldeiradeoliveira>
2024-03-04 11:17:10 -0300

    Criação do Projeto e Instalação do Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,10 +13,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule,
+     
+    
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/components/components.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { angularMaterialModule } from "../angular-material/angular-material.module";
+
+@NgModule({
+    declarations:[],
+    imports:[angularMaterialModule],
+    exports:[],
+})
+export class ComponentsModule{}
