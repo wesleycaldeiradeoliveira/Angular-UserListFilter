877bdb846d43ad8b0bfa6725282165f684403437
wesleycaldeiradeoliveira <wesleycaldeiradeoliveira>
2024-03-04 12:06:11 -0300

    Criação da Estrutura de Container Base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
