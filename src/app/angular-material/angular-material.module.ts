2237fd63c00d1e7ca9f2a9f29b132d7c235e307c
wesleycaldeiradeoliveira <wesleycaldeiradeoliveira>
2024-03-07 12:00:33 -0300

    Criação do Layout do Componente de Filtro

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
+
 import { MatListModule } from '@angular/material/list';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
 @NgModule({
-  imports: [MatListModule,MatDividerModule],
-  exports: [MatListModule,MatDividerModule],
+  imports: [
+    MatListModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatButtonModule,
+  ],
+  exports: [
+    MatListModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatButtonModule,
+  ],
 })
 export class angularMaterialModule {}
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { angularMaterialModule } from '../angular-material/angular-material.module';
 import { UserDetailsComponent } from './user-details/user-details.component';
-
+import { FilterComponent } from './filter/filter.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @NgModule({
-  declarations: [UserDetailsComponent],
-  imports: [angularMaterialModule],
-  exports: [UserDetailsComponent],
+  declarations: [UserDetailsComponent, FilterComponent],
+  imports: [angularMaterialModule, CommonModule,FormsModule],
+  exports: [UserDetailsComponent, FilterComponent],
 })
 export class ComponentsModule {}
