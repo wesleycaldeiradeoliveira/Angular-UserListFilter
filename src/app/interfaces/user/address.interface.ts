7af286492479f0ac83ef4636038febab31af3cbc
wesleycaldeiradeoliveira <wesleycaldeiradeoliveira>
2024-03-12 19:21:58 -0300

    Criação do Pipe de Endereço

--- src/app/interfaces/user/address.interface.ts
+++ src/app/interfaces/user/address.interface.ts
@@ -1,7 +1,7 @@
 export interface IAddress {
   rua: string;
-    numero: number;
-    cidade: string;
-    estado: string;
-    pais: string;
+  numero: number;
+  cidade: string;
+  estado: string;
+  pais: string;
 }
--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PhonePipe } from './phone.pipe';
+import { AddressPipe } from './address.pipe';
 
 @NgModule({
   declarations: [
     PhonePipe,
+    AddressPipe,
   ],
   exports: [
     PhonePipe,
+    AddressPipe,
   ],
 })
 export class PipesModel {}
