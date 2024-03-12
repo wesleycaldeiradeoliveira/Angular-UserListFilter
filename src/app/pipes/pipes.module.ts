109485e2b5e48ecc0f98a0a396e733da7e0ea904
wesleycaldeiradeoliveira <wesleycaldeiradeoliveira>
2024-03-12 19:49:04 -0300

    Criação do Pipe de Status

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PhonePipe } from './phone.pipe';
 import { AddressPipe } from './address.pipe';
+import { StatusPipe } from './status.pipe';
 
 @NgModule({
   declarations: [
     PhonePipe,
     AddressPipe,
+    StatusPipe,
   ],
   exports: [
     PhonePipe,
     AddressPipe,
+    StatusPipe,
   ],
 })
 export class PipesModel {}
