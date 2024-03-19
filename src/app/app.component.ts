860c303ea88dcbd33c839440fd381cff4bb9ea89
wesleycaldeiradeoliveira <wesleycaldeiradeoliveira>
2024-03-19 13:59:21 -0300

    Criação da Lógica de Filtro por Nome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,48 @@ import { IFilterOptions } from './interfaces/filter-options.interface';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit{
-
+export class AppComponent implements OnInit {
   usersList: IUser[] = [];
   userSelected: IUser = {} as IUser;
+  usersListFiltered: IUser[] = [];
   showUserDetails: boolean = false;
+
   ngOnInit() {
-   setTimeout(() => {
-    this.usersList = UserList
-   }, 1000);
+    setTimeout(() => {
+      this.usersList = UserList;
+      this.usersListFiltered = this.usersList;
+    }, 1000);
   }
 
   onUserSelected(user: IUser) {
     this.userSelected = user;
     this.showUserDetails = true;
   }
-  onFilter(filterOptions: IFilterOptions){
+  onFilter(filterOptions: IFilterOptions) {
     console.log(filterOptions);
+
+    this.usersListFiltered = this.filterUsersList(
+      filterOptions,
+      this.usersList
+    );
+  }
+  filterUsersList(filterOptions: IFilterOptions, usersList: IUser[]): IUser[] {
+    let filteredList: IUser[] = [];
+
+    filteredList = this.filterUsersListByName(filterOptions.name, usersList);
+
+    return filteredList;
+  }
+
+  filterUsersListByName(name: string | undefined, usersList: IUser[]): IUser[] {
+    const NAME_NOT_TYPPED = name === undefined;
+
+    if(NAME_NOT_TYPPED) {
+      return usersList;
+    }
+
+    const filteredList = usersList.filter((user) => user.nome.toLowerCase().includes(name.toLowerCase()));
+
+    return filteredList;
   }
 }
